a9ebfe54fb0ea4235cb8ea3caa93fa547d917b51
divadnauj-GB <juan.david.guerrero.balaguera@gmail.com>
2026-04-24 16:21:07 -0500

    added uart check in tb

--- test/test.py
+++ test/test.py
@@ -9,6 +9,9 @@ logging.getLogger("cocotb.tb.uart0_tx").setLevel(logging.WARNING)
 logging.getLogger("cocotb.tb.uart0_rx").setLevel(logging.WARNING)
 
 
+GOLDEN_OUTPUT="FreeRTOs-check.txt"
+OUTPUT_SIM="FreeRTOs.txt"
+
 #logging.basicConfig(level=logging.DEBUG)
 
 async def uart_send(clk,dev, msg=[]):
@@ -21,16 +24,19 @@ async def uart_send(clk,dev, msg=[]):
 class monitor_uart():
     def __init__(self,tx):
         self.uart_sink = UartSink(tx, baud=115200, bits=8)
+        with open(OUTPUT_SIM,'w') as fp:
+            pass
 
     async def start(self):
         while True:
             ch = await self.uart_sink.read()
             print(ch.decode("utf-8"), end="")
-            with open("FreeRTOs.txt","a") as fp:
+            with open(OUTPUT_SIM,"a") as fp:
                 fp.write(ch.decode("utf-8"))
 
 @cocotb.test()
 async def test_uart(dut):
+
     dut._log.setLevel(logging.DEBUG)
     clock = dut.clk
     uart_source = UartSource(dut.uart0_rx, baud=115200, bits=8)
@@ -40,10 +46,16 @@ async def test_uart(dut):
 
     await ClockCycles(dut.clk, 20000 * 10)
    
-    # await uart_send(dut.clk, uart_source, "g01000000")
+    await uart_send(dut.clk, uart_source, "g01000000")
 
+    await ClockCycles(dut.clk, 20000 * 50)
     # await ClockCycles(dut.clk, 20000 * 1000)
    
 
+    with open(GOLDEN_OUTPUT,"r") as Gfp:
+        expected_output=Gfp.readlines()
 
+    with open(OUTPUT_SIM,"r") as Rfp:
+        sim_output=Rfp.readlines()
 
+    assert(expected_output==sim_output)
